e68947e43273f9b8906fd16906bad64189042a15
srinuvasulupurini <srinuvasulu.purini@gmail.com>
2022-12-18 06:54:45 +0530

    updated node details in jenkins file

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -4,7 +4,7 @@ public class FizzBuzz {
 
     public String play(int number) {
 
-        if (number == 0) throw new IllegalArgumentException("test for github hook for jenkins");
+        if (number == 0) throw new IllegalArgumentException("test updated for github hook for jenkins");
         if (number % 3 == 0) return "Fizz";
         if (number % 5 == 0) return "Buzz";
 
